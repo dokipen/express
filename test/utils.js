ea18ffdc78c12b743f9016ff61f2d181d4dc8536
Bob Corsaro <bob@embed.ly>
2012-03-23 22:12:57 -0400

    Fix broken code

--- test/utils.js
+++ test/utils.js
@@ -166,14 +166,14 @@ describe('utils.accepts(type, str)', function(){
       utils.accepts(['text/javascript', 'application/javascript', 'text/xml',
              'application/xml', 'text/html'],
              'text/html,application/xhtml+xml,' +
-             'application/xml;q=0.9,*/*;q=0.8');
+             'application/xml;q=0.9,*/*;q=0.8')
         .should.be.eql('text/html');
     })
     it('should return the best match', function(){
       utils.accepts(['text/javascript', 'application/javascript', 'text/xml',
              'application/xml', 'html'],
              'text/html,application/xhtml+xml,' +
-             'application/xml;q=0.9,*/*;q=0.8');
+             'application/xml;q=0.9,*/*;q=0.8')
         .should.be.eql('html');
     })
   })
